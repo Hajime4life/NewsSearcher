e83dcae274ce7c6092c32b122f8c2ae332bdbca3
Hajime4life <doguziagerman@gmail.com>
2025-06-25 18:27:12 +0300

    Тест коммит 2

--- NewsSearcher/Config/Secrets.swift
+++ NewsSearcher/Config/Secrets.swift
@@ -6,6 +6,5 @@ private enum Secrets {
     /// Regístrese previamente en newsapi.org e inserte aquí su clave API para que la aplicación funcione.
     /// Enregistrez-vous préalablement sur newsapi.org et insérez votre clé API ici pour que l'application fonctionne.
     /// Melden Sie sich vorab auf newsapi.org an und fügen Sie hier Ihren API-Schlüssel ein, damit die App funktioniert.
-    
     static let newsApiKey = "api-key"
 }
